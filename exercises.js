bc02a640fecf25a811ae17c1830bbf23cc59c6c3
patwyzlic <patrickwyzlic@gmail.com>
2022-05-16 14:20:42 -0400

    Commit 2 - Talking Heads: 77

--- exercises.js
+++ exercises.js
@@ -23,8 +23,12 @@ const album1 = {
 
 // 1. Retrieve the string "Sire" from album1, and save it in a sensibly named
 //    variable.
+let theLabel = album1.albumDetails.label;
+console.log(theLabel);
 
 // 2. Change the title of album1 from "Talking Heads" to "Talking Heads: 77"
+album1.title = "Talking Heads: 77"
+console.log(album1.title);
 
 const album2 = {
   title: "More Songs About Buildings and Food",
